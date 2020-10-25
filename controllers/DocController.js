b937827cb779a54a39c2fdb5482f7bfe1b9577e9
bayu <bayuyudo20@gmail.com>
2020-10-25 14:40:23 +0700

    menambahkan modul dokumen, modul approvel dan tindak lanjut

--- controllers/DocController.js
+++ controllers/DocController.js
@@ -84,4 +84,147 @@ docRouter.get('/all', async (req,res) => {
    
 });
 
+
+//read documen status disetujui
+docRouter.get('/approvel', async (req,res) => {
+
+    //header apabila akan melakukan akses
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    
+    //verifikasi jwt
+    jwt.verify(token, Conf.secret, async function(err, decoded) {
+        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        const jabatan = decoded.user.jabatan;
+            if( jabatan !== ''){
+                const doc =  await Doc.find({ "status":1 });
+
+                if(doc && doc.length !== 0) {
+                    res.json(doc)
+                } else {
+                    res.status(404).json({
+                        message: 'Doc not found'
+                    });
+                }
+            } else {
+                
+                res.status(500).send(`${decoded.user.username} Tidak Memiliki Wewenang`);
+            }
+        })
+});
+
+
+//READ user by ID
+docRouter.get('/all/:id', async (req,res) => {
+
+    //header apabila akan melakukan akses
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    
+    //verifikasi jwt
+    jwt.verify(token, Conf.secret, async function(err, decoded) {
+        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        const jabatan = decoded.user.jabatan;
+            if( jabatan !== ''){
+                const doc = await Doc.findById(req.params.id);
+                    if(doc) {
+                        res.json(doc)
+                    } else {
+                        res.status(404).json({
+                            message: 'Doc not found'
+                        });
+                    }             
+            } else {
+                
+                res.status(500).send(`${decoded.user.username} Tidak Memiliki Wewenang`);
+            }
+        })
+
+
+
+    
+});
+
+// Konfirmasi surat
+docRouter.put('/all/update/:id', function (req, res) {
+    //header apabila akan melakukan akses
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    
+    //verifikasi jwt
+    jwt.verify(token, Conf.secret, async function(err, decoded) {
+        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        const jabatan = decoded.user.jabatan;
+            if( jabatan == '0'){
+                const {kategori, nomer, redaksi, tujuan, tanggal,status,keterangan} = req.body;
+                const doc = await Doc.findById(req.params.id);
+                
+                if (doc.status == 1) {
+                    console.log(doc.status)
+                    res.status(500).send(`Tidak bisa diubah karena Sudah Disetujui`);
+
+                } else {
+                    doc.kategori = kategori;
+                    doc.nomer = nomer;
+                    doc.redaksi = redaksi;
+                    doc.tanggal = tanggal
+                    doc.tujuan = tujuan;
+                    doc.status = status;
+                    doc.keterangan = keterangan;
+                    const updateStatus = await doc.save()
+
+                    res.send(updateStatus);
+                }
+            } else {
+                const {kategori, nomer, redaksi, tujuan, tanggal,keterangan} = req.body;
+                const doc = await Doc.findById(req.params.id);
+
+                if (doc.status == 1) {
+                    console.log(doc.status)
+                    res.status(500).send(`Tidak bisa diubah karena Sudah Disetujui`);
+
+                } else {
+                    doc.kategori = kategori;
+                    doc.nomer = nomer;
+                    doc.redaksi = redaksi;
+                    doc.tanggal = tanggal
+                    doc.tujuan = tujuan;
+                    doc.keterangan = keterangan;
+                    const updateStatus = await doc.save()
+
+                    res.send(updateStatus);
+                }
+            }
+    })
+});
+
+//Delete doc By ID
+docRouter.delete('/all/:id', async (req,res) => {
+
+    //header apabila akan melakukan akses
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+
+    //verifikasi jwt
+    jwt.verify(token, Conf.secret, async function(err, decoded) {
+        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        const jabatan = decoded.user.jabatan;
+            if( jabatan !== ''){
+
+    const doc = await Doc.findById(req.params.id);
+
+    if (doc) {
+        await doc.remove();
+        res.json({
+            message: 'Doc removed'
+        })
+    } else {
+        res.status(404).json({
+            message: 'Doc not found' 
+        })       
+    }
+    }
+})
+});
+
 export default docRouter;
